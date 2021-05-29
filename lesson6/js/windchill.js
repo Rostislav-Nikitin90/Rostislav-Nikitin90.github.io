5c19d3ca8faa42f049e296786427d247c2825668
rostislav-nikitin90 <rosnikitin90@gmail.com>
2021-05-29 22:27:55 +0200

    Initial Commit

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,12 @@
+const tempF = document.querySelector('.temp').innerHTML;
+const speed = document.querySelector('.windspeed').innerHTML;
+let f;
+
+if (tempF <= 50 && tempF >= 3) {
+  f = 35.74 + (0.6215 * tempF) - 35.75 * (Math.pow(speed, 0.16)) + ((0.4275 * tempF) * (Math.pow(speed, 0.16)));
+  document.querySelector('.windchill').innerHTML = f.toFixed(0) + ' °F';
+} 
+
+else {
+  document.querySelector('.windchill').innerHTML = 'N/A';
+}
