4255a477b46316b1fc3f33bf68ddef358f21f189
rostislav-nikitin90 <rosnikitin90@gmail.com>
2021-07-22 07:33:56 +0200

    Initial Commit

--- lesson13-14/js/budvachamber-script.js
+++ lesson13-14/js/budvachamber-script.js
@@ -12,3 +12,13 @@ const fulldate = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full" }).format(now);
 
 currentdate.innerHTML = `<em>${fulldate}</em>`;
+
+//to display a list of local business organizations either in a "grid view" (cards in multiple columns) or in a "list view" (a single column) - Directory Page
+$('button').on('click',function(e) {
+    if($(this).hasClass('list')) {
+        $('#container ul').removeClass('grid').addClass('list');
+    }
+    else if ($(this).hasClass('grid')) {
+        $('#container ul').removeClass('list').addClass('grid');
+    }
+});
